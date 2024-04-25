9cefcb17d72f06e5d99f80507f94e013d33fc58d
benarba mohammed <tewfikhotspur@gmail.com>
2024-04-25 13:49:05 +0100

    add functionality to display less text info

--- src/projects/Tours/components/Tour.jsx
+++ src/projects/Tours/components/Tour.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
+
 export default function Tour({ tour }) {
   const { image, name, info, price } = tour;
+  const [text, useText] = useState(info.substring(0, 200));
+  function handleClick() {
+    if (text.length > 200) {
+      useText(info.substring(0, 200));
+    } else {
+      useText(info);
+    }
+  }
   return (
     <article className="tour-article">
       <img src={image} alt={name} />
       <div className="tour-article-info">
         <h2>{name}</h2>
-        <p>{info}</p>
+        <p>
+          {text}
+          <button className="text-btn" onClick={() => handleClick()}>
+            {text.length <= 200 ? "..read more" : "show less"}
+          </button>
+        </p>
         <span>${price}</span>
-        <button>not interested</button>
+        <button className="btn">not interested</button>
       </div>
     </article>
   );
