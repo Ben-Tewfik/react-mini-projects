6219a24a83e4e3039620de64fd5265187a4aa29e
benarba mohammed <tewfikhotspur@gmail.com>
2024-04-26 17:40:29 +0100

    change logic to add ability for the user to read more

--- src/projects/Tours/components/Tour.jsx
+++ src/projects/Tours/components/Tour.jsx
@@ -2,23 +2,16 @@ import { useState } from "react";
 
 export default function Tour({ tour }) {
   const { image, name, info, price } = tour;
-  const [text, useText] = useState(info.substring(0, 200));
-  function handleClick() {
-    if (text.length > 200) {
-      useText(info.substring(0, 200));
-    } else {
-      useText(info);
-    }
-  }
+  const [read, setRead] = useState(false);
   return (
     <article className="tour-article">
       <img src={image} alt={name} />
       <div className="tour-article-info">
         <h2>{name}</h2>
         <p>
-          {text}
-          <button className="text-btn" onClick={() => handleClick()}>
-            {text.length <= 200 ? "..read more" : "show less"}
+          {read ? info : `${info.substring(0, 200)}...`}
+          <button className="text-btn" onClick={() => setRead(!read)}>
+            {read ? "show less" : "read more"}
           </button>
         </p>
         <span>${price}</span>
