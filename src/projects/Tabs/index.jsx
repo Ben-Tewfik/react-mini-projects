b5285e557dd09be50c826effed7b9fed26d9fce1
benarba mohammed <tewfikhotspur@gmail.com>
2024-04-30 13:46:21 +0100

    setup logic to change the array element when clic on different button

--- src/projects/Tabs/index.jsx
+++ src/projects/Tabs/index.jsx
@@ -1,3 +1,38 @@
+import { useState } from "react";
+import tabs from "../../utils/tabsData";
+import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+
 export default function Tabs() {
-  return <div>Tabs</div>;
+  const [value, setValue] = useState(0);
+  const { company, dates, duties, id, title } = tabs[value];
+  return (
+    <section className="tabs-container">
+      <div className="tab-buttons-container">
+        {tabs.map((item, index) => {
+          return (
+            <button key={item.id} onClick={() => setValue(index)}>
+              {item.company}
+            </button>
+          );
+        })}
+      </div>
+      <div className="tabs-info-container">
+        <article className="tab-container" key={id}>
+          <h1 className="tab-title">{title}</h1>
+          <span className="tab-company">{company}</span>
+          <p className="tab-date">{dates}</p>
+          <div className="tab-duties">
+            {duties.map((duty, index) => {
+              return (
+                <div className="duty-container" key={index}>
+                  <MdOutlineKeyboardDoubleArrowRight className="duty-icon" />
+                  <p className="duty-desc">{duty}</p>
+                </div>
+              );
+            })}
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 }
