49e2dc78b1725e80d1ddbf61d29639ff7fffb52d
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-02 12:47:43 +0100

    style the slider

--- src/projects/Quote Slider/Slider.jsx
+++ src/projects/Quote Slider/Slider.jsx
@@ -1,4 +1,29 @@
+import people from "../../utils/sliderData";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
 import "./style/slider.css";
-export default function () {
-  return <div>Slider</div>;
+console.log(people);
+export default function Slider() {
+  return (
+    <section className="slider-container">
+      {people.map(person => {
+        const { id, image, name, quote, title } = person;
+        return (
+          <article className="slide" key={id}>
+            <img src={image} alt={name} className="slide-img" />
+            <h5 className="slide-name">{name}</h5>
+            <p className="slide-title">{title}</p>
+            <p className="slide-quote">{quote}</p>
+            <FaQuoteRight className="slide-icon" />
+          </article>
+        );
+      })}
+      <button className="next-btn">
+        <FiChevronRight />
+      </button>
+      <button className="prev-btn">
+        <FiChevronLeft />
+      </button>
+    </section>
+  );
 }
