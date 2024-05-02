d8908f5e669029d1451ac1ee4ee08afba7db2391
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-02 13:40:29 +0100

    add logic for prev and next btn and change the css style to suit the logic

--- src/projects/Quote Slider/Slider.jsx
+++ src/projects/Quote Slider/Slider.jsx
@@ -2,14 +2,42 @@ import people from "../../utils/sliderData";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 import "./style/slider.css";
+import { useEffect, useState } from "react";
 console.log(people);
 export default function Slider() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    if (currentIndex < 0) {
+      setCurrentIndex(people.length - 1);
+    }
+    if (currentIndex > people.length - 1) {
+      setCurrentIndex(0);
+    }
+  }, [currentIndex]);
+  useEffect(() => {
+    const slider = setInterval(() => {
+      setCurrentIndex(currentIndex + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [currentIndex]);
   return (
     <section className="slider-container">
-      {people.map(person => {
+      {people.map((person, index) => {
         const { id, image, name, quote, title } = person;
+        let position = "nextSlide";
+        if (index === currentIndex) {
+          position = "active";
+        }
+        if (
+          index === currentIndex - 1 ||
+          (currentIndex === 0 && index === people.length - 1)
+        ) {
+          position = "lastSlide";
+        }
         return (
-          <article className="slide" key={id}>
+          <article className={`${position} slide`} key={id}>
             <img src={image} alt={name} className="slide-img" />
             <h5 className="slide-name">{name}</h5>
             <p className="slide-title">{title}</p>
@@ -18,10 +46,16 @@ export default function Slider() {
           </article>
         );
       })}
-      <button className="next-btn">
+      <button
+        className="next-btn"
+        onClick={() => setCurrentIndex(currentIndex + 1)}
+      >
         <FiChevronRight />
       </button>
-      <button className="prev-btn">
+      <button
+        className="prev-btn"
+        onClick={() => setCurrentIndex(currentIndex - 1)}
+      >
         <FiChevronLeft />
       </button>
     </section>
