bf708dcc166e96db021af8dccb1cdcb68ae37c77
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-06 11:31:02 +0100

    add logic to display the number of paragraphs selected in the form

--- src/projects/Lorem Ipsum/LoremIpsum.jsx
+++ src/projects/Lorem Ipsum/LoremIpsum.jsx
@@ -1,11 +1,17 @@
 import "./style/loremIpsum.css";
 import text from "../../utils/LoremIpsumData";
-console.log(text);
+import { useState } from "react";
 export default function LoremIpsum() {
+  const [number, setNumber] = useState(1);
+  const [data, setData] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setData(text.slice(0, number));
+  }
   return (
     <section className="lorem-section">
       <h4 className="lorem-title">tired of boring lorem ipsum ?</h4>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="number" className="lorem-label">
           paragraphs:
         </label>
@@ -16,10 +22,20 @@ export default function LoremIpsum() {
           max={8}
           step={1}
           min={1}
+          value={number}
+          onChange={e => setNumber(e.target.value)}
         />
         <button className="lorem-btn">generate</button>
       </form>
-      <article className="lorem-paragraph"></article>
+      <article className="lorem-paragraph">
+        {data.map((item, index) => {
+          return (
+            <p key={index} className="lorem-text">
+              {item}
+            </p>
+          );
+        })}
+      </article>
     </section>
   );
 }
