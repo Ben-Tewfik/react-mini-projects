d1a564bb8ab4f9c3ec9b9644f01d8ebe8ef48eff
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-06 13:33:49 +0100

    create color generator project starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Colors from "./projects/Color Generator/Colors";
 import LoremIpsum from "./projects/Lorem Ipsum/LoremIpsum";
 
 function App() {
   return (
     <>
-      <LoremIpsum />
+      <Colors />
     </>
   );
 }
