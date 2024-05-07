a15f55f8334071e45a399e70a577fed7bc72c538
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-07 10:54:58 +0100

    structure and style the form section of the color project

--- src/projects/Color Generator/Colors.jsx
+++ src/projects/Color Generator/Colors.jsx
@@ -1,3 +1,39 @@
+import React, { useState } from "react";
+import "./style/colors.css";
 export default function Colors() {
-  return <div>Colors</div>;
+  const [error, setError] = useState(false);
+  const [color, setColor] = useState("");
+  const [list, setList] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(color);
+  }
+  return (
+    <main className="colors-main">
+      <section className="colors-form-section">
+        <h1>color generator</h1>
+        <form onSubmit={handleSubmit} className="colors-form">
+          <input
+            type="color"
+            value={color}
+            onChange={e => setColor(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="#F15025"
+            value={color}
+            onChange={e => setColor(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="colors-btn"
+            style={{ backgroundColor: `${color}` }}
+          >
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors-section"></section>
+    </main>
+  );
 }
