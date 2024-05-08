5150c8c1f582d63960dc0be3d7f9bd3dd3c5c4ae
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-08 23:34:23 +0100

    create grocery form structure and style it

--- src/projects/Grocery Bud/Grocery.jsx
+++ src/projects/Grocery Bud/Grocery.jsx
@@ -1,4 +1,15 @@
 import "./style/grocery.css";
 export default function Grocery() {
-  return <div>Grocery</div>;
+  return (
+    <main className="grocery-main-container">
+      <section className="grocery-container">
+        <h4 className="grocery-title">Grocery Bud</h4>
+        <form className="grocery-form">
+          <input type="text" placeholder="Add you item" />
+          <button className="grocery-btn">Add item</button>
+        </form>
+        <div className="list-container"></div>
+      </section>
+    </main>
+  );
 }
