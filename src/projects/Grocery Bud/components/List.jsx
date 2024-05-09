c451741428151a755341697ffc0895b4cc622afd
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-09 10:47:30 +0100

    add structure and style to the list component

--- src/projects/Grocery Bud/components/List.jsx
+++ src/projects/Grocery Bud/components/List.jsx
@@ -1,3 +1,18 @@
-export default function List() {
-  return <div>List</div>;
+import { FaEdit } from "react-icons/fa";
+import { FaTrashAlt } from "react-icons/fa";
+
+export default function List({ title, id }) {
+  return (
+    <article className="grocery-item-container">
+      <p className="grocery-item-title">{title}</p>
+      <div className="grocery-btns-container">
+        <button className="grocery-edit-btn">
+          <FaEdit />
+        </button>
+        <button className="grocery-delete-btn">
+          <FaTrashAlt />
+        </button>
+      </div>
+    </article>
+  );
 }
