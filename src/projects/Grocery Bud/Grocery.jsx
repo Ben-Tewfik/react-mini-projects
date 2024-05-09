3e3347d257322cf4f28574a23944189844c65be7
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-09 10:48:23 +0100

    add state to display user input into the list component

--- src/projects/Grocery Bud/Grocery.jsx
+++ src/projects/Grocery Bud/Grocery.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import "./style/grocery.css";
+import List from "./components/List";
 export default function Grocery() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    const id = new Date().getTime().toString();
+    setList([...list, { title: name, id }]);
+    setName("");
+  }
   return (
     <main className="grocery-main-container">
       <section className="grocery-container">
         <h4 className="grocery-title">Grocery Bud</h4>
-        <form className="grocery-form">
-          <input type="text" placeholder="Add you item" />
+        <form className="grocery-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add your item"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
           <button className="grocery-btn">Add item</button>
         </form>
-        <div className="list-container"></div>
+        <div className="list-container">
+          {list.map((item, index) => {
+            return <List {...item} key={index} />;
+          })}
+        </div>
+        <button className="clear-btn">clear all</button>
       </section>
     </main>
   );
