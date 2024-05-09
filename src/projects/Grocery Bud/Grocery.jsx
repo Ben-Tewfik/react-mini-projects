77c72367c37c8f9cc255a09e041ec78cb8809737
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-09 12:33:15 +0100

    add logic for delete btns and edit btn and local storage

--- src/projects/Grocery Bud/Grocery.jsx
+++ src/projects/Grocery Bud/Grocery.jsx
@@ -1,15 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./style/grocery.css";
 import List from "./components/List";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function getLocalStorage() {
+  const list = JSON.parse(localStorage.getItem("list"));
+  if (list) {
+    return list;
+  } else {
+    return [];
+  }
+}
+
 export default function Grocery() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
+  const [isEdited, setIsEdited] = useState(false);
+  const [editId, setEditId] = useState(null);
   function handleSubmit(e) {
     e.preventDefault();
-    const id = new Date().getTime().toString();
-    setList([...list, { title: name, id }]);
-    setName("");
+    if (!name) {
+      toast.error("Please Enter an Item", { position: "top-center" });
+    } else if (name && isEdited) {
+      setList(
+        list.map(item => {
+          if (editId === item.id) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      toast.success("Item Edited Successfully", { position: "top-center" });
+      setEditId(null);
+      setIsEdited(false);
+      setName("");
+    } else {
+      const id = new Date().getTime().toString();
+      setList([...list, { title: name, id }]);
+      toast.success("Item Added Successfully", { position: "top-center" });
+      setName("");
+    }
   }
+  function removeItem(id) {
+    setList(list.filter(item => item.id !== id));
+    toast.success("Item Deleted", { position: "top-center" });
+  }
+  function editItem(id) {
+    const specificItem = list.find(item => item.id === id);
+    setIsEdited(true);
+    setEditId(id);
+    setName(specificItem.title);
+  }
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
   return (
     <main className="grocery-main-container">
       <section className="grocery-container">
@@ -21,14 +66,27 @@ export default function Grocery() {
             value={name}
             onChange={e => setName(e.target.value)}
           />
-          <button className="grocery-btn">Add item</button>
+          <button className="grocery-btn">
+            {isEdited ? "Edit Item" : "Add item"}
+          </button>
         </form>
-        <div className="list-container">
-          {list.map((item, index) => {
-            return <List {...item} key={index} />;
-          })}
-        </div>
-        <button className="clear-btn">clear all</button>
+        {list.length > 0 && (
+          <div className="list-container">
+            {list.map((item, index) => {
+              return (
+                <List
+                  {...item}
+                  key={index}
+                  removeItem={removeItem}
+                  editItem={editItem}
+                />
+              );
+            })}
+          </div>
+        )}
+        <button className="clear-btn" onClick={() => setList([])}>
+          {list.length > 0 && "clear all"}
+        </button>
       </section>
     </main>
   );
--- src/projects/Grocery Bud/components/List.jsx
+++ src/projects/Grocery Bud/components/List.jsx
@@ -1,15 +1,15 @@
 import { FaEdit } from "react-icons/fa";
 import { FaTrashAlt } from "react-icons/fa";
 
-export default function List({ title, id }) {
+export default function List({ title, id, removeItem, editItem }) {
   return (
     <article className="grocery-item-container">
       <p className="grocery-item-title">{title}</p>
       <div className="grocery-btns-container">
-        <button className="grocery-edit-btn">
+        <button className="grocery-edit-btn" onClick={() => editItem(id)}>
           <FaEdit />
         </button>
-        <button className="grocery-delete-btn">
+        <button className="grocery-delete-btn" onClick={() => removeItem(id)}>
           <FaTrashAlt />
         </button>
       </div>
