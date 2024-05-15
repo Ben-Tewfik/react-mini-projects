e885359efc815140435de81f90838d4e9fe466f0
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-15 09:23:51 +0100

    create navbar component structure and style the nav header in mobile screen

--- src/projects/Strapi Submenu/components/Navbar.jsx
+++ src/projects/Strapi Submenu/components/Navbar.jsx
@@ -1,3 +1,16 @@
+import { FaBars } from "react-icons/fa";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="strapi-nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <h1>strapi</h1>
+          <button className="strapi-btn">
+            <FaBars />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
