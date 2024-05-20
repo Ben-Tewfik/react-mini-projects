a436919ebc6dee00ec3547c92f48d02ad9bea344
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-20 12:37:18 +0100

    create structure and style sidebar

--- src/projects/Strapi Submenu/components/Sidebar.jsx
+++ src/projects/Strapi Submenu/components/Sidebar.jsx
@@ -1,3 +1,36 @@
+import { FaTimes } from "react-icons/fa";
+import sublinks from "../../../utils/StrapiData";
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="strapi-sidebar">
+      <div className="strapi-sidebar-container">
+        <button className="sidebar-close-btn">
+          <FaTimes />
+        </button>
+        <div className="links-container">
+          {sublinks.map(link => {
+            const { pageId, page, links } = link;
+            return (
+              <article key={pageId}>
+                <h3>{page}</h3>
+                <ul className="sublinks-container">
+                  {links.map(link => {
+                    const { id, label, icon, url } = link;
+                    return (
+                      <li key={id}>
+                        <a href={url}>
+                          {icon}
+                          {label}
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
 }
