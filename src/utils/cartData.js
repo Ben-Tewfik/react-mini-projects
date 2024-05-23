15d4a8d2866fbe123137a338d0a1a080d6592b47
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-23 11:50:56 +0100

    create cartData file

---
+++ src/utils/cartData.js
@@ -0,0 +1,23 @@
+export const cartItems = [
+  {
+    id: 1,
+    title: "Samsung Galaxy S7",
+    price: 599.99,
+    img: "https://i.ebayimg.com/images/g/z8cAAOSwtkhhEWx5/s-l960.webp",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "google pixel ",
+    price: 499.99,
+    img: "https://lh3.googleusercontent.com/KaLIFYVg9298b8jv33H3pagRaAz4lCQxrQz-goMEsiTuCmUf2Ood9ktkzgjpotkMuRcAMimOV2RfN7vBZVmnInf5wcwUNsRZpw",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKGZRN0XL-KTBo4G37h04K7vyZfhxnWGNfEDboz8ZdnA&s",
+    amount: 1,
+  },
+];
