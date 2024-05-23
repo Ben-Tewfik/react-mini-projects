d15fc44979015b874e3a839aabe6086656bc9956
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-23 12:55:54 +0100

    create cart main structure and style it

--- src/projects/Phone Cart/components/Cart.jsx
+++ src/projects/Phone Cart/components/Cart.jsx
@@ -1,3 +1,24 @@
+import { cartItems } from "../../../utils/cartData";
+import CartItem from "./CartItem";
+
 export default function Cart() {
-  return <div>Cart</div>;
+  return (
+    <section className="cart-container">
+      <header className="cart-header">
+        <h2>your bag</h2>
+      </header>
+      <article className="cart-main">
+        {cartItems.map(item => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </article>
+      <footer className="cart-footer">
+        <hr />
+        <h5>
+          total <span>$0</span>
+        </h5>
+        <button className="cart-clear-btn">clear cart</button>
+      </footer>
+    </section>
+  );
 }
--- src/projects/Phone Cart/components/CartItem.jsx
+++ src/projects/Phone Cart/components/CartItem.jsx
@@ -1,3 +1,22 @@
-export default function CartItem() {
-  return <div>CartItem</div>;
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+export default function CartItem({ id, amount, img, price, title }) {
+  return (
+    <div className="single-item">
+      <img src={img} alt="" className="single-item-img" />
+      <div className="single-item-info">
+        <h5>{title}</h5>
+        <span>${price}</span>
+        <button className="remove-btn">remove</button>
+      </div>
+      <div className="single-item-toggle-container">
+        <button className="increase-items-btn">
+          <IoIosArrowUp />
+        </button>
+        <span className="single-item-amount">{amount}</span>
+        <button className="decrease-items-btn">
+          <IoIosArrowDown />
+        </button>
+      </div>
+    </div>
+  );
 }
