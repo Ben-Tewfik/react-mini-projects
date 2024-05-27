f47c1363c479eb993ace6588d0170feccacfda6c
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-27 12:38:36 +0100

    create data file contains three object to iterate over just for initial logic

---
+++ src/utils/faselSliderData.js
@@ -0,0 +1,25 @@
+import movie1 from "../projects/Fasel Movie Slider/assets/img-1.webp";
+import movie2 from "../projects/Fasel Movie Slider/assets/img-2.jpg";
+import movie8 from "../projects/Fasel Movie Slider/assets/img-8.webp";
+const movies = [
+  {
+    img: movie1,
+    title: "darkness of man 2024",
+    categories: ["action", "thriller"],
+    desc: "Russell Hatch, an Interpol agent who takes on the role of father to Jayden, the son of an informant killed in a routine raid gone wrong.",
+  },
+  {
+    img: movie2,
+    title: "The Ministry of Ungentlemanly Warfare 2024",
+    categories: ["drama", "war"],
+    desc: "The British Army recruits a small group of highly skilled soldiers to strike German forces behind enemy lines during World War II.",
+  },
+  {
+    img: movie8,
+    title: "Atlas 2024",
+    categories: ["action", "drama", "adventure"],
+    desc: "In a bleak future, artificial intelligence. The soldier has decided that the only way to end the war is to end humanity.",
+  },
+];
+
+export default movies;
