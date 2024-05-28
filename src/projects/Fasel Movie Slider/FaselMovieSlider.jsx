6040ff9ca90d47f1bb22bff009e1d40c74589bce
benarba mohammed <tewfikhotspur@gmail.com>
2024-05-28 13:55:38 +0100

    add more slides and style them and add next and prev btn logic

--- src/projects/Fasel Movie Slider/FaselMovieSlider.jsx
+++ src/projects/Fasel Movie Slider/FaselMovieSlider.jsx
@@ -1,8 +1,23 @@
 import "./style/faselMovieSlider.css";
+import { FaPlay } from "react-icons/fa6";
+import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
 import movies from "../../utils/faselSliderData";
 import { useEffect, useState } from "react";
 export default function FaselMovieSlider() {
   const [imageIndex, setImageIndex] = useState(0);
+  console.log("out", imageIndex);
+  function showNextSlide() {
+    setImageIndex(index => {
+      if (index === movies.length - 1) return movies.length - 1;
+      return index + 1;
+    });
+  }
+  function showPrevSlide() {
+    setImageIndex(index => {
+      if (imageIndex <= 0) return 0;
+      return index - 1;
+    });
+  }
 
   useEffect(() => {
     // cleanup function
@@ -11,36 +26,48 @@ export default function FaselMovieSlider() {
         return setImageIndex(0);
       }
       return setImageIndex(imageIndex + 1);
-    }, 3000);
+    }, 5000);
   }, [imageIndex]);
   return (
     <section className="fasel-slider-container">
       <div className="fasel-slider-img-container">
-        {movies.map((movie, index) => {
-          const { img, title, desc, categories } = movie;
+        {movies.map(movie => {
+          const { img, title, desc, categories, poster } = movie;
           return (
-            <>
+            <div key={title} className="slider-img-container">
               <img
                 src={img}
                 alt={`${title} image`}
                 className={`fasel-slider-img`}
                 style={{ translate: `-${imageIndex * 100}%` }}
-                key={title}
               />
-              <article className="fasel-slider-img-info">
-                <h2 className="fasel-slider-img-title">{title}</h2>
-                <ul className="fasel-slider-img-genres-container">
-                  {categories.map(genre => {
-                    return (
-                      <li key={genre} className="fasel-slider-img-genre">
-                        {genre}
-                      </li>
-                    );
-                  })}
-                </ul>
-                <p className="fasel-slider-img-desc">{desc}</p>
+              <article
+                className="fasel-slider-img-info"
+                style={{ translate: `-${imageIndex * 100}%` }}
+              >
+                <img
+                  src={poster}
+                  alt="poster img"
+                  className="fasel-slider-poster"
+                />
+                <div className="movie-desc-container">
+                  <h2 className="fasel-slider-img-title">{title}</h2>
+                  <ul className="fasel-slider-img-genres-container">
+                    {categories.map(genre => {
+                      return (
+                        <li key={genre} className="fasel-slider-img-genre">
+                          {genre}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                  <p className="fasel-slider-img-desc">{desc}</p>
+                  <button className="fasel-slider-info-btn">
+                    watch now <FaPlay />
+                  </button>
+                </div>
               </article>
-            </>
+            </div>
           );
         })}
       </div>
@@ -59,6 +86,12 @@ export default function FaselMovieSlider() {
           );
         })}
       </div>
+      <button className="next-slide-btn" onClick={showNextSlide}>
+        <MdNavigateNext />
+      </button>
+      <button className="prev-slide-btn" onClick={showPrevSlide}>
+        <MdNavigateBefore />
+      </button>
     </section>
   );
 }
--- src/utils/faselSliderData.js
+++ src/utils/faselSliderData.js
@@ -1,21 +1,72 @@
 import movie1 from "../projects/Fasel Movie Slider/assets/img-1.webp";
 import movie2 from "../projects/Fasel Movie Slider/assets/img-2.jpg";
+import movie3 from "../projects/Fasel Movie Slider/assets/img-3.jpg";
+import movie4 from "../projects/Fasel Movie Slider/assets/img-4.jpg";
+import movie5 from "../projects/Fasel Movie Slider/assets/img-5.jpg";
+import movie6 from "../projects/Fasel Movie Slider/assets/img-6.jpg";
+import movie7 from "../projects/Fasel Movie Slider/assets/img-7.jpg";
 import movie8 from "../projects/Fasel Movie Slider/assets/img-8.webp";
+import poster1 from "../projects/Fasel Movie Slider/assets/poster-1.jpg";
+import poster2 from "../projects/Fasel Movie Slider/assets/poster-2.jpg";
+import poster3 from "../projects/Fasel Movie Slider/assets/poster-3.jpg";
+import poster4 from "../projects/Fasel Movie Slider/assets/poster-4.jpg";
+import poster5 from "../projects/Fasel Movie Slider/assets/poster-5.jpg";
+import poster6 from "../projects/Fasel Movie Slider/assets/poster-6.jpg";
+import poster7 from "../projects/Fasel Movie Slider/assets/poster-7.jpg";
+import poster8 from "../projects/Fasel Movie Slider/assets/poster-8.jpg";
 const movies = [
   {
     img: movie1,
+    poster: poster1,
     title: "darkness of man 2024",
     categories: ["action", "thriller"],
     desc: "Russell Hatch, an Interpol agent who takes on the role of father to Jayden, the son of an informant killed in a routine raid gone wrong.",
   },
   {
     img: movie2,
+    poster: poster2,
     title: "The Ministry of Ungentlemanly Warfare 2024",
     categories: ["drama", "war"],
     desc: "The British Army recruits a small group of highly skilled soldiers to strike German forces behind enemy lines during World War II.",
   },
   {
+    img: movie3,
+    poster: poster3,
+    title: "Godzilla x Kong: The New Empire 2024",
+    categories: ["action", "sci-fi", "adventure"],
+    desc: "Two ancient titans, Godzilla and Kong, clash in an epic battle as the humans uncover their tangled origins and connection to the mysteries of Skull Island.",
+  },
+  {
+    img: movie4,
+    poster: poster4,
+    title: "You Cant Run Forever 2024",
+    categories: ["thriller", "drama"],
+    desc: "A teenage girl suffering from anxiety due to a tragic event from her past finds herself stalked into the woods by a sociopath on a murderous rampage.",
+  },
+  {
+    img: movie5,
+    poster: poster5,
+    title: "Challengers 2024",
+    categories: ["drama", "romance", "sport"],
+    desc: "Tashi, a former tennis prodigy turned coach, turned her husband into a champion. But to break the losing streak, he needs to face his former best friend and Tashi's ex-boyfriend.",
+  },
+  {
+    img: movie6,
+    poster: poster6,
+    title: "Boy Kills World 2023",
+    categories: ["thriller", "action"],
+    desc: "A fever dream action film about Bowie, a deaf person with a vivid imagination. When his family is murdered, he is trained by a mysterious shaman to suppress his childish imagination and become an instrument of death.",
+  },
+  {
+    img: movie7,
+    poster: poster7,
+    title: "The Fall Guy 2024",
+    categories: ["action", "drama", "comedy"],
+    desc: "A struggling stuntman must find the missing star of his ex-girlfriend's hit movie.",
+  },
+  {
     img: movie8,
+    poster: poster8,
     title: "Atlas 2024",
     categories: ["action", "drama", "adventure"],
     desc: "In a bleak future, artificial intelligence. The soldier has decided that the only way to end the war is to end humanity.",
