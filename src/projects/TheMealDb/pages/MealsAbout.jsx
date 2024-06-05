6912628907069792757af0b2ac06d31d7f1f2bbe
benarba mohammed <tewfikhotspur@gmail.com>
2024-06-05 22:13:54 +0100

    create about component and style it with navbar

---
+++ src/projects/TheMealDb/pages/MealsAbout.jsx
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <section className="meals-about">
+      <h1 className="meals-about-title">about us</h1>
+      <p className="meals-about-text">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore quod
+        itaque eligendi unde repellendus ad, autem quis iste, neque doloremque
+        voluptatum illum iure dolor esse placeat ducimus tenetur consequuntur
+        sed consectetur, magnam accusamus sequi obcaecati corrupti. Beatae
+        praesentium sed optio. Accusamus debitis odit maxime sequi libero magni
+        dicta modi sint corrupti quidem officia consequuntur nisi, officiis
+        eaque, quod atque veritatis.
+      </p>
+    </section>
+  );
+}
